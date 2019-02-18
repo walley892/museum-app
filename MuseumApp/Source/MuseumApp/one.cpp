b94a1167e9e62d6deebb01b4920c156b3cdb4786
Evan <evanwall@buffalo.edu>
2019-02-18 10:42:57 -0800

    Consolidated user stories

---
+++ MuseumApp/Source/MuseumApp/one.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "one.h"
+
+// Sets default values
+Aone::Aone()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Aone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Aone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void Aone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+

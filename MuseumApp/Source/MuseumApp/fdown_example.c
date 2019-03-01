523f0c70002ff19c092559456396bd57318dad24
Asher Lieber <asherlie@buffalo.edu>
2019-03-01 14:01:10 -0500

    added error handling to example fdown file

--- MuseumApp/Source/MuseumApp/fdown_example.c
+++ MuseumApp/Source/MuseumApp/fdown_example.c
@@ -7,7 +7,10 @@
 
 
 int main(int a, char** b){
-      if(a < 3)return 1;
+      if(a < 3){
+            puts("enter file url and download location");
+            return 1;
+      }
       int ret;
       char e_b[51] = {0};
       if(!(ret = dl_file(b[1], b[2], e_b)))

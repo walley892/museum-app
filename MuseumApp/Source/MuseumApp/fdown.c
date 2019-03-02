2929e5412df02f8c1ce6aa6e1e68bbbbba8ac87d
Asher Lieber <asherlie@buffalo.edu>
2019-03-02 01:45:10 -0500

    simplified file download code

--- MuseumApp/Source/MuseumApp/fdown.c
+++ MuseumApp/Source/MuseumApp/fdown.c
@@ -11,18 +11,15 @@
 _Bool dl_file(char* in_f, char* out_f, char* err_buf){
       FILE* fp = fopen(out_f, "wb");
       if(!fp)return 0;
-      CURL* curl;
-      CURLcode ret;
-      if((curl = curl_easy_init())){
-            curl_easy_setopt(curl, CURLOPT_URL, in_f);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-            ret = curl_easy_perform(curl);
-            if(err_buf)strncpy(err_buf, curl_easy_strerror(ret), 50);
-            curl_easy_cleanup(curl);
-            fclose(fp);
-      }
-      else return 0;
-      // on success ret == 0
+      CURL* curl; CURLcode ret;
+      if(!(curl = curl_easy_init()))return 0;
+      curl_easy_setopt(curl, CURLOPT_URL, in_f);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+      ret = curl_easy_perform(curl);
+      if(err_buf)strncpy(err_buf, curl_easy_strerror(ret), 50);
+      curl_easy_cleanup(curl);
+      fclose(fp);
+      // ret == 0 on success
       return !ret;
 }
--- MuseumApp/Source/MuseumApp/fdown.h
+++ MuseumApp/Source/MuseumApp/fdown.h
@@ -1,1 +1,2 @@
+/* returns download success */
 _Bool dl_file(char* in_f, char* out_f, char* err_buf);

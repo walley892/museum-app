182a3008e6c4fa1f9baf7cb1dcf8bb4952f662f7
Asher Lieber <asherlie@buffalo.edu>
2019-03-08 11:58:24 -0500

    added qr decoding functionality

---
+++ MuseumApp/Source/MuseumApp/qr_ed.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "qrlib/quirc.h"
+
+/* this file has been moved from tests to lib */
+#include "qrlib/dbgutil.h"
+
+#define SCR_RES 200, 500
+
+uint8_t* decode_qr(char* fname){
+      struct quirc* qr = quirc_new();
+      // not enough memory
+      if(!qr)goto uhoh;
+
+      quirc_resize(qr, SCR_RES);
+
+      uint8_t* pic;
+      int w, h;
+      pic = quirc_begin(qr, &w, &h);
+
+      // need to populate pic[w,h]
+      // load_* return 0 on success
+      if(check_if_png(fname)){
+            if(load_png(qr, fname))goto uhoh;
+      }
+      else if(load_jpeg(qr, fname))goto uhoh;
+
+      quirc_end(qr);
+
+      uint8_t* ret = NULL;
+
+      struct quirc_code code;
+      struct quirc_data data;
+      quirc_decode_error_t err;
+
+      quirc_extract(qr, 0, &code);
+      if((err = quirc_decode(&code, &data)))
+           ret = NULL;
+      else ret = data.payload;
+
+      quirc_destroy(qr);
+
+      return ret;
+
+      uhoh:
+            puts("something went wrong...");
+            return NULL;
+}
+
+int main(int a, char** b){
+      printf("%s\n", decode_qr(b[1]));
+      return 0;
+}

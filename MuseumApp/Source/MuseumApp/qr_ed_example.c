7517cc58ddfe0270d9a53f4e20421cccea3681e1
asherlie <lieber.asher@gmail.com>
2019-03-08 12:19:19 -0500

    updated qr_ed_example.c build instructions

--- MuseumApp/Source/MuseumApp/qr_ed_example.c
+++ MuseumApp/Source/MuseumApp/qr_ed_example.c
@@ -1,11 +1,11 @@
 /* build instructions:
  * gcc qr_ed_example.c qr_ed.c qrlib/quirc.c qrlib/decode.c
  * qrlib/dbgutil.c qrlib/identify.c qrlib/version_db.c -o qr_ex
- * -ljpeg -lpng
+ * -ljpeg -lpng -lm
  *
  *  or just
  *
- *  gcc qr_ed_example.c qr_ed.c qrlib/*.c
+ *  gcc qr_ed_example.c qr_ed.c qrlib/*.c -ljpeg -lpng -lm
  */
 
 #include <stdio.h>
@@ -13,6 +13,7 @@
 #include "qr_ed.h"
 
 int main(int a, char** b){
+      if(a < 2)return 1;
       printf("%s\n", decode_qr(b[1]));
       return 0;
 }

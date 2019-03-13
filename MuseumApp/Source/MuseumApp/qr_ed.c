497a89d3445a1298e5cc999595904b78570c082d
asherlie <lieber.asher@gmail.com>
2019-03-13 16:47:58 -0400

    add get_dimensions_png(), which uses libpng to parse png files and return an int* with width, height

--- MuseumApp/Source/MuseumApp/qr_ed.c
+++ MuseumApp/Source/MuseumApp/qr_ed.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <png.h>
 
 #include "qrlib/quirc.h"
 /* this file has been moved from tests to lib */
@@ -7,6 +8,34 @@
 #include "qr_ed.h"
 
 #define SCR_RES 200, 500
+
+// returns width, height
+int* get_dimensions_png(char* fname){
+      int* ret = NULL;
+      FILE* fp = fopen(fname, "rb");
+      if(!fp)return NULL;
+
+      png_structp img = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+      if(!img)goto cleanup_fp;
+      png_infop inf = png_create_info_struct(img);
+      if(!inf)goto cleanup_fp;
+
+      // ... libpng documentation told me to http://www.libpng.org/pub/png/libpng-1.2.5-manual.html
+      if(setjmp(png_jmpbuf(img)))goto cleanup_fp;
+
+      png_init_io(img, fp);
+      png_read_info(img, inf);
+
+      ret = malloc(sizeof(int)*2);
+      ret[0] = png_get_image_width(img, inf);
+      ret[1] = png_get_image_height(img, inf);
+
+      cleanup_inf:
+      png_destroy_read_struct(&img, &inf, NULL);
+      cleanup_fp:
+      fclose(fp);
+      return ret;
+}
 
 uint8_t* decode_qr(char* fname){
       struct quirc* qr = quirc_new();
@@ -38,6 +67,7 @@ uint8_t* decode_qr(char* fname){
       if((err = quirc_decode(&code, &data)))
            ret = NULL;
       else ret = data.payload;
+      // printf("got \"%s\"/%i\n", data.payload, data.payload);
 
       quirc_destroy(qr);
 

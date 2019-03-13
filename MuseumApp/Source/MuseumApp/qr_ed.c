66b3140c6fa9ec31d5e262c3c48c29cca47178dd
asherlie <lieber.asher@gmail.com>
2019-03-13 17:00:46 -0400

    updated get_dimensions_png to write to a buffer provided by the caller, gdp() is now used by decode_qr to check dimensions of image

--- MuseumApp/Source/MuseumApp/qr_ed.c
+++ MuseumApp/Source/MuseumApp/qr_ed.c
@@ -7,13 +7,10 @@
 
 #include "qr_ed.h"
 
-#define SCR_RES 200, 500
-
 // returns width, height
-int* get_dimensions_png(char* fname){
-      int* ret = NULL;
+_Bool get_dimensions_png(char* fname, int* wh){
       FILE* fp = fopen(fname, "rb");
-      if(!fp)return NULL;
+      if(!fp)return 0;
 
       png_structp img = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
       if(!img)goto cleanup_fp;
@@ -26,23 +23,32 @@ int* get_dimensions_png(char* fname){
       png_init_io(img, fp);
       png_read_info(img, inf);
 
-      ret = malloc(sizeof(int)*2);
-      ret[0] = png_get_image_width(img, inf);
-      ret[1] = png_get_image_height(img, inf);
+      wh[0] = png_get_image_width(img, inf);
+      wh[1] = png_get_image_height(img, inf);
 
       cleanup_inf:
       png_destroy_read_struct(&img, &inf, NULL);
       cleanup_fp:
       fclose(fp);
-      return ret;
+      return img && fp && inf;
 }
 
 uint8_t* decode_qr(char* fname){
       struct quirc* qr = quirc_new();
+
+      _Bool png;
+      // limiting scope of dim
+      {
+      // jpeg dimensions will always be 200, 500 bc... idk...
+      int dim[2] = {200, 500};
+      if((png = check_if_png(fname)))
+            get_dimensions_png(fname, dim);
+      printf("dim: %i, %i\n", dim[0], dim[1]);
+      quirc_resize(qr, dim[0], dim[1]);
+      }
+
       // not enough memory
       if(!qr)return NULL;
-
-      quirc_resize(qr, SCR_RES);
 
       uint8_t* pic;
       int w, h;
@@ -50,7 +56,7 @@ uint8_t* decode_qr(char* fname){
 
       // need to populate pic[w,h]
       // load_* return 0 on success
-      if(check_if_png(fname)){
+      if(png){
             if(load_png(qr, fname))return NULL;
       }
       else if(load_jpeg(qr, fname))return NULL;

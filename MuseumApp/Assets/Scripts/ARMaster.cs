d880ef4d608674c4aaf07e511a4ad5657d1eb60c
Evan <evanwall@buffalo.edu>
2019-03-13 23:10:24 -0700

    Created classes for the AR scene controller and for the models we're putting in the camera feed

---
+++ MuseumApp/Assets/Scripts/ARMaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARMaster : MonoBehaviour
+{
+    private bool _spawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        List<AugmentedImage> trackedImages = new List<AugmentedImage>();
+        Session.GetTrackables<AugmentedImage>(trackedImages, TrackableQueryFilter.Updated);
+
+        foreach(AugmentedImage img in trackedImages)
+        {
+            if(img.TrackingState == TrackingState.Tracking)
+            {
+                if (!_spawned)
+                {
+                    GameObject obj = new GameObject();
+                    AugmentedModel model = obj.AddComponent<AugmentedModel>();
+                    model.Initalize(img, 0);
+                    _spawned = true;
+                }
+            }
+        }
+    }
+}
---
+++ MuseumApp/Assets/Scripts/AugmentedModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class AugmentedModel : MonoBehaviour
+{
+    private AugmentedImage _image;
+    private int _modelId;
+    
+    public void Initalize(AugmentedImage img, int modelId)
+    {
+        _image = img;
+        _modelId = modelId;
+    }
+
+    
+    void Start()
+    {
+        Anchor anc = _image.CreateAnchor(_image.CenterPose);
+        gameObject.transform.localPosition = Vector3.zero;
+    }
+
+    
+    void Update()
+    {
+        if (_image == null || _image.TrackingState != TrackingState.Tracking)
+        {
+            return;
+        }
+
+        //gameObject.transform.position = Vector3.zero;
+    }
+
+
+}

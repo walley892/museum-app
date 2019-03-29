7230135a6b8c46ffc7d7a2c229a3fd4d9a5aefd1
Asher Lieber <asherlie@buffalo.edu>
2019-03-28 22:33:46 -0400

    updated header file according to change

--- MuseumApp/Source/MuseumApp/qr_ed.h
+++ MuseumApp/Source/MuseumApp/qr_ed.h
@@ -3,4 +3,4 @@
 /* returns success - width, height are written into wh[0], wh[1] */
 _Bool get_dimensions_png(char* fname, int* wh);
 /* returns NULL on failure, NULL terminated array of qr code text on success */
-uint8_t** decode_qr(char* fname);
+uint8_t** decode_qr(char* fname, int* n_qr);

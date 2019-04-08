82f2e573dae6741d7a5367ccdbf5d3de86b0acc5
jasjeev4 <jasjeev4@gmail.com>
2019-04-08 13:04:09 -0400

    Change port to port 80 for server traffic

--- MuseumServer/server.js
+++ MuseumServer/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 require('./app/routes')(express, app);
 

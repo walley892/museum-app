019a26921781b96d38e4fbc767229070ce3d427a
Evan <evanwall@buffalo.edu>
2019-04-11 14:12:29 -0700

    implemeted model rotation

--- MuseumApp/Assets/Scripts/AugmentedReality/AugmentedModel.cs
+++ MuseumApp/Assets/Scripts/AugmentedReality/AugmentedModel.cs
@@ -8,7 +8,9 @@ public class AugmentedModel : MonoBehaviour
     private Trackable _base;
 
     private int _modelId;
-    
+
+    private float _yRot;
+
     void Start()
     {
         Anchor anc = _base.CreateAnchor(_base.GetCenterPose());
@@ -16,12 +18,14 @@ public class AugmentedModel : MonoBehaviour
         gameObject.transform.parent = anc.transform;
         gameObject.transform.localPosition = Vector3.zero;
         gameObject.transform.localScale = Vector3.one;
+
+        _yRot = 0;
     }
 
     
     void Update()
     {
-        
+        rotateLeft(0.7f);
     }
 
     public void setBase(Trackable b)
@@ -33,4 +37,17 @@ public class AugmentedModel : MonoBehaviour
     {
         _modelId = modelId;
     }
+
+    public void rotateLeft(float degs)
+    {
+        _yRot += degs;
+        gameObject.transform.localRotation = Quaternion.Euler(0, _yRot, 0);
+    }
+
+    public void rotateRight(float degs)
+    {
+        rotateLeft(-degs);
+    }
+
+
 }
---
+++ MuseumApp/Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+        ModelManager m = new LocalModelManager("artifacts/");
+        GameObject g = m.createModel(0);
+        //g.GetComponent<Renderer>().material.SetTexture("_MainTex", m.getTexture(0));
+        //obj.GetComponent<Renderer>().material.SetTexture("_MainTex", m.getTexture(0));
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}

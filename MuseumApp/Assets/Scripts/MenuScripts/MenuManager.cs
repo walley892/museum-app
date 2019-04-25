2b891bc60c0703930db85086a79d4682c8927754
Vialythen <jjknauf@buffalo.edu>
2019-04-25 09:11:12 -0400

    Language Toggle
    The language toggle is now implimented into all static text fields across all scenes with rough translations from google translate, to be reviewed by a native Greek speaker.

--- MuseumApp/Assets/Scripts/MenuScripts/MenuManager.cs
+++ MuseumApp/Assets/Scripts/MenuScripts/MenuManager.cs
@@ -10,7 +10,7 @@ public class MenuManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-
+        updateText();
     }
 
     // Update is called once per frame
@@ -23,14 +23,65 @@ public class MenuManager : MonoBehaviour
         MenuState.toggleLanguage();
 
         if (MenuState.english()){
+            GameObject.Find("WelcomeText").GetComponent<Text>().text = "Kallinikeio";
+            GameObject.Find("MuseumText").GetComponent<Text>().text = "Municipal Museum of Athienou";
+
+            GameObject.Find("TourButton").GetComponentInChildren<Text>().text = "Tour";
+            GameObject.Find("LibraryButton").GetComponentInChildren<Text>().text = "Library";
+            GameObject.Find("SettingsButton").GetComponentInChildren<Text>().text = "Settings";
             GameObject.Find("LanguageButton").GetComponentInChildren<Text>().text = "Ελληνικά";
+            GameObject.Find("ExitButton").GetComponentInChildren<Text>().text = "Exit";
+            GameObject.Find("CreditsButton").GetComponentInChildren<Text>().text = "Credits";
         }
         else {
+            GameObject.Find("WelcomeText").GetComponent<Text>().text = "Καλλινίκειο";
+            GameObject.Find("MuseumText").GetComponent<Text>().text = "Δημοτικό Μουσείο Αθηένου";
+
+            GameObject.Find("TourButton").GetComponentInChildren<Text>().text = "Περιοδεία";
+            GameObject.Find("LibraryButton").GetComponentInChildren<Text>().text = "Βιβλιοθήκη";
+            GameObject.Find("SettingsButton").GetComponentInChildren<Text>().text = "Ρυθμίσεις";
             GameObject.Find("LanguageButton").GetComponentInChildren<Text>().text = "Εnglish";
+            GameObject.Find("ExitButton").GetComponentInChildren<Text>().text = "Εξοδος";
+            GameObject.Find("CreditsButton").GetComponentInChildren<Text>().text = "Πιστώσεις";
         }
 
 
     }
 
+
+    public void updateText(){
+
+        if (MenuState.english()){
+            if (GameObject.Find("WelcomeText") != null) GameObject.Find("WelcomeText").GetComponent<Text>().text = "Kallinikeio";
+            if (GameObject.Find("MuseumText") != null) GameObject.Find("MuseumText").GetComponent<Text>().text = "Municipal Museum of Athienou";
+
+            if (GameObject.Find("TourText") != null) GameObject.Find("TourText").GetComponent<Text>().text = "Tour";
+            if (GameObject.Find("LibraryText") != null) GameObject.Find("LibraryText").GetComponent<Text>().text = "Library";
+            if (GameObject.Find("SettingsText") != null) GameObject.Find("SettingsText").GetComponent<Text>().text = "Settings";
+            if (GameObject.Find("LanguageText") != null) GameObject.Find("LanguageText").GetComponent<Text>().text = "Ελληνικά";
+            if (GameObject.Find("ExitText") != null) GameObject.Find("ExitText").GetComponent<Text>().text = "Exit";
+            if (GameObject.Find("CreditsText") != null) GameObject.Find("CreditsText").GetComponent<Text>().text = "Credits";
+
+            if (GameObject.Find("BackButton") != null) GameObject.Find("BackButton").GetComponentInChildren<Text>().text = "Back";
+        
+        }
+        else {
+            if (GameObject.Find("WelcomeText") != null) GameObject.Find("WelcomeText").GetComponent<Text>().text = "Καλλινίκειο";
+            if (GameObject.Find("MuseumText") != null) GameObject.Find("MuseumText").GetComponent<Text>().text = "Δημοτικό Μουσείο Αθηένου";
+
+            if (GameObject.Find("TourText") != null) GameObject.Find("TourText").GetComponent<Text>().text = "Περιοδεία";
+            if (GameObject.Find("LibraryText") != null) GameObject.Find("LibraryText").GetComponent<Text>().text = "Βιβλιοθήκη";
+            if (GameObject.Find("SettingsText") != null) GameObject.Find("SettingsText").GetComponent<Text>().text = "Ρυθμίσεις";
+            if (GameObject.Find("LanguageText") != null) GameObject.Find("LanguageText").GetComponent<Text>().text = "English";
+            if (GameObject.Find("ExitText") != null) GameObject.Find("ExitText").GetComponent<Text>().text = "Εξοδος";
+            if (GameObject.Find("CreditsText") != null) GameObject.Find("CreditsText").GetComponent<Text>().text = "Πιστώσεις";
+
+            if (GameObject.Find("BackButton") != null) GameObject.Find("BackButton").GetComponentInChildren<Text>().text = "Πίσω";
+
+        }
+
+
+
+    }
 
 }
--- MuseumApp/Assets/Scripts/MenuScripts/libraryHandler.cs
+++ MuseumApp/Assets/Scripts/MenuScripts/libraryHandler.cs
@@ -78,10 +78,4 @@ public class libraryHandler : MonoBehaviour
         Destroy(GameObject.Find("Library Artifact"));
     }
 
-
-
-    public void LoadByIndex(int sceneIndex){
-        SceneManager.LoadScene(sceneIndex);
-    }
-
 }

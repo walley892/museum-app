ca0132c09f6218ed2a70dc1c722fd2b6a36aa6c4
Vialythen <jjknauf@buffalo.edu>
2019-04-25 08:13:55 -0400

    UI Traversability & Features
    Credits & Settings page created, and basic functionality for a language toggle have been added.
    
    A back button has also been implimented into the AR scene, meaning all menu states are now fully traversable, forwards and back.
    
    Additionally, the old and obsolete UI and Library scene have been deleted.

--- MuseumApp/Assets/Scripts/MenuScripts/MenuManager.cs
+++ MuseumApp/Assets/Scripts/MenuScripts/MenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -9,7 +10,6 @@ public class MenuManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-        //MenuState;
 
     }
 
@@ -18,5 +18,19 @@ public class MenuManager : MonoBehaviour
     {
         
     }
+
+    public void toggleLanguage(){
+        MenuState.toggleLanguage();
+
+        if (MenuState.english()){
+            GameObject.Find("LanguageButton").GetComponentInChildren<Text>().text = "Ελληνικά";
+        }
+        else {
+            GameObject.Find("LanguageButton").GetComponentInChildren<Text>().text = "Εnglish";
+        }
+
+
+    }
+
 
 }
